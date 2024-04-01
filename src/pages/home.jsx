366b89632744533fee0a0bc5a5f153e23a98ad11
EduRodgz <edurodgz.usmc@icloud.com>
2024-04-01 12:13:00 -0500

    1st commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,13 @@
+import "./home.css";
+
+
+function Home(){
+    return (
+        <div className="window">
+            <h1>Why should YOU hire ME?</h1>
+            <p>Eduardo Rodriguez Jr is the name, frontend developing is my game.</p>
+        </div>
+    );
+}
+
+export default Home;
